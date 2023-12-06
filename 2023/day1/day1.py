5136b4746a6bd2c95e2554bc3b84660e6a246401
Lorenitz <lorena.g.saar@gmail.com>
2023-12-06 23:25:21 +0100

    day 1 part 2

--- 2023/day1/day1.py
+++ 2023/day1/day1.py
@@ -1,16 +1,58 @@
 
-def calibration_part_1 (input):
+num_dict = {
+    "one": 1, 
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "1": 1, 
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9
+    }
 
+def convert_line_to_numbers(line):
+    slice_start = 0
+    line_result = []
+    
+    for i in range(len(line)+1):
+        slice_to_analyze = line[i:]
+        
+        # Replace written numbers with numerical values
+        for word, number in num_dict.items():
+            if slice_to_analyze.startswith(word):
+                line_result.append(number)
+                # slice_start = i
+                
+    return line_result    
+ 
+
+def calibration_part_1(input):
+    
+    
     calibration_value = input.split("\n")
+   # print(input)
+   # print(calibration_value)
+    
+
     numbers_list = []
-
-
     for item in calibration_value:
-        numbers = []
-        for char in item:
-            if char.isdigit():
-                numbers.append(int(char))
-        numbers_list.append(numbers)
+        numbers_list.append(convert_line_to_numbers(item))
+    #     print(convert_line_to_numbers(item))
+    #     numbers = []
+    #     for char in item:
+    #         if char.isdigit():
+    #             numbers.append(int(char))
+    #     numbers_list.append(numbers)
  
 
     final_list = []
@@ -27,7 +69,10 @@ def calibration_part_1 (input):
         
         final_list.append(int(str(first_value) + str(second_value)))
         
-    print(sum(final_list))
+    return sum(final_list)
+    
+    
+    
 
 
 if __name__ == '__main__':
@@ -36,4 +81,5 @@ if __name__ == '__main__':
         lines = f.readlines()
         test_input = ''.join(lines)
 
+#print(convert_line_to_numbers(test_input))
 print(calibration_part_1(test_input))
--- 2023/day1/day1_test.py
+++ 2023/day1/day1_test.py
@@ -1,5 +1,5 @@
 import sys
-from day1 import calibration_part_1
+from day1 import calibration_part_1, convert_line_to_numbers
 
 
 
@@ -8,3 +8,5 @@ with open('test_input.txt') as f:
     test_input = ''.join(lines)
     
 print(calibration_part_1(test_input))
+
+#print(convert_line_to_numbers(test_input))
